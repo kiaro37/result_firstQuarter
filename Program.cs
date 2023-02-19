73d1cfe10dcdfbf9add6ef80f21282c32f3acff0
Evgeniy <iva.sakharov.e@gmail.com>
2023-02-19 22:24:00 +0300

    Create metods

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача. Написать программу,
+// которая из имеющегося массива строк сформирует массив из строк, 
+// длина которых меньше либо равна 3 символам.
+
+
+string[] CreateArray(int size)
+{
+    string[] array = new string[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Console.ReadLine(); //записываем в массив числа введенные пользователем
+    }
+    return array;
+}
+
+void PrintArray(string[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write(arr[i] + ",");
+        else Console.Write(arr[i]);
+    }
+    Console.Write("]");
+}
+
+
+Console.WriteLine("Какое количество чисел вы хотите ввести?");
+int size = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите числа через нажатие клавиши Enter");
+
+string[] array = CreateArray(size);
+PrintArray(array);
+
+Console.Write(" ---> ");
